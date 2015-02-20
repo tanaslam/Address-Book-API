9877bd0ceb332b557280c278800cb15ee3eb9ef9
Tanveer Aslam <tan_aslam@yahoo.co.uk>
2015-02-20 00:33:26 +0000

    Added .env file

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send(cool());
+	
+	var out = ''
+	var times = process.env.TIMES || 5
+
+	console.log('times: ' + times);
+	
+	for (i = 0; i < times; i++)
+		out+=cool() + ' ';
+  	response.send(out);
 });
 
 app.listen(app.get('port'), function() {
