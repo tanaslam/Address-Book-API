eb0083cc7ef9cadac6121dcbd5ddcff2352c306c
Tanveer Aslam <tan_aslam@yahoo.co.uk>
2015-02-23 19:23:15 +0000

    Added API routing

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 var express = require('express');
 var app = express();
-var cool = require('cool-ascii-faces');
+var bodyParser = require('body-parser');
 
-app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.urlencoded({extented: true}));
+app.use(bodyParser.json());
+
 app.use(express.static(__dirname + '/public'));
+app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(request, response) {
-	
-	var out = ''
-	var times = process.env.TIMES || 5
+var router = express.Router();
 
-	console.log('times: ' + times);
-	
-	for (i = 0; i < times; i++)
-		out+=cool() + ' ';
-  	response.send(out);
+router.get('/', function(request, response) {
+	response.json({message: 'address-book API is running'});
 });
+
+router.get('/contacts', function(request, response) {
+	response.json({"contacts":[{"name":"Tanveer","phone":"0207 834521"},{"name":"Aslam","phone":"0787 834521"},{"name":"Billy","phone":"+44787834521"}]});
+})
+
+app.use('/api', router);
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
